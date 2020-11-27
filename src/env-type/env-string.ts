6315e793fda6a2dedacba36f192d53cf4fbcdfa0
milos <milos.bugarinovic@gmail.com>
2020-11-27 21:27:02 +0100

    refactor: use convert strategy

--- src/env-type/env-string.ts
+++ src/env-type/env-string.ts
@@ -1,18 +1,25 @@
-import { Env } from '../env'
-import { BaseEnvStorage } from './base-env-storage'
+// import { Env } from '../env'
+// import { BaseEnvStorage } from './base-env-storage'
+import { ConvertStrategy } from './convert-strategy'
 
-export class EnvString extends BaseEnvStorage<string> {
-  constructor(env: Env) {
-    super(env)
-  }
+// export class EnvString extends BaseEnvStorage<string> {
+//   constructor(env: Env) {
+//     super(env)
+//   }
+//
+//   protected _convertValue(stringOrUndefined?: string): string | undefined {
+//     const stringValue = stringOrUndefined ?? ''
+//     return stringValue.trim() || this._defaultValue
+//   }
+//
+//   public default(defaultValue: string): EnvString {
+//     this._setDefault(defaultValue)
+//     return this
+//   }
+// }
 
-  protected _convertValue(stringOrUndefined?: string): string | undefined {
-    const stringValue = stringOrUndefined ?? ''
-    return stringValue.trim() || this._defaultValue
-  }
-
-  public default(defaultValue: string): EnvString {
-    this._setDefault(defaultValue)
-    return this
+export class EnvString implements ConvertStrategy<string> {
+  public convert(str: string): string | undefined {
+    return str
   }
 }
