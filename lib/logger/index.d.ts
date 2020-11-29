d3cd8e0fdd75ff6eb7ecd255ff8e2e685a4afa12
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2020-11-29 16:48:38 +0100

    refactor: .gitignore update, push missing files

---
+++ lib/logger/index.d.ts
@@ -0,0 +1,4 @@
+export * from './console-logging';
+export * from './logger-strategy';
+export * from './none-logger';
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/util/index.d.ts
@@ -0,0 +1,2 @@
+export * from './string-util';
+//# sourceMappingURL=index.d.ts.map
