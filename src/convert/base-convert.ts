15f7676beedebbd75309876c27948bdf3576e6fc
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2021-03-17 13:04:31 +0100

    fix: remove npm-force-resolutions

--- src/convert/base-convert.ts
+++ src/convert/base-convert.ts
@@ -11,15 +11,19 @@ export class BaseConvert {
   public get string(): EnvType<string> {
     return new EnvType<string>({ convertStrategy: new ToString(), env: this.__env })
   }
+
   public get boolean(): EnvType<boolean> {
     return new EnvType<boolean>({ convertStrategy: new ToBoolean(), env: this.__env })
   }
+
   public get number(): EnvType<number> {
     return new EnvType<number>({ convertStrategy: new ToNumber(), env: this.__env })
   }
+
   public json<T>(): EnvType<T> {
     return new EnvType<T>({ convertStrategy: new ToJson<T>(), env: this.__env })
   }
+
   public get base64(): EnvType<string> {
     return new EnvType<string>({ convertStrategy: new Base64ToString(), env: this.__env })
   }
